11730c743149116a9ad984d01a341bbd6dfb5998
Qusai Alhanaktah <57313315+Qusai-alhanaktah@users.noreply.github.com>
2021-03-02 18:25:58 +0200

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -2,8 +2,17 @@
 
 const express = require('express');
 const bcrypt = require('bcryptjs');
+var admin = require("firebase-admin");
+const cron = require("node-cron");
+var serviceAccount = require('./simple-app-416f2-firebase-adminsdk-gb9xl-e1cedd7a5a.json');
 const routes = express.Router();
 const Users = require('./auth.js');
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://simple-app-416f2-default-rtdb.firebaseio.com"
+});
 
 // CALCULATION API
 
@@ -171,4 +180,58 @@ function basicAuth (req, res, next) {
       });
   }
 }
+// Cron Job 
+
+var message = {
+  notification: {
+    title: 'Miss You :)',
+    body: 'We will be glad when see you activate the app 🔥🔥'
+  },
+  data: {},
+  android: {
+    notification: {
+      sound: 'default'
+    }
+  },
+  apns: {
+    payload: {
+      aps: {
+        sound: 'default'
+      }
+    }
+  },
+  token: 'efzPTQgfSHuNee-z4esT4N:APA91bHXnEse492QxnLb8krKF45yDYwyPobRw8Kkqj3z0T4_sJ_sFW8cwjbK02tYvyRASe9Cgnq75XrwuJTWLbFks--rBcZpgJYiFi3vc7M7aPwRPFH5_ReX8O6asOHbSHrDtAQpaX5G'
+}
+
+let task = cron.schedule(`*/1 * * * *`, () => {
+  admin.messaging().send(message)
+  .then(response => {
+    console.log('Message sent Sucessfully', response);
+  })
+  .catch(error => {
+    console.log('error sending a message', error);
+  });
+}, {
+  scheduled: false,
+});
+
+routes.get('/api/v1/scheduleNotificaions', (req, res) => {
+    try {
+      task.start();
+      res.status(200).send('The notification will send');
+    } catch (e) {
+      console.log(e);
+      res.status(500).send('Server Erorr');
+    }
+});
+routes.get('/api/v1/stopScheduleNotificaions', (req, res) => {
+    try {
+      task.stop();
+      res.status(200).send('The notification schedual is stop');
+    } catch (e) {
+      console.log(e);
+      res.status(500).send('Server Erorr');
+    }
+});
+
 module.exports = routes;
